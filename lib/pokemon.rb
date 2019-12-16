5afea2153d550186ff28c705316c0654b5b4fdce
Pancho Labrador <plabradorjr@gmail.com>
2019-12-16 02:38:15 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,12 @@
 class Pokemon
 
   attr_accessor :name, :type
-  attr_reader :id 
+  attr_reader :id
 
   def initialize (id = nil, name, type)
     @id = id
     @name = name
     @type = type
-
   end
 
 end
